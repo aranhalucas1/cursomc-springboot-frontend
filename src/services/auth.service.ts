3d197214ccfc43d4f12b2ffd726d1fe41767095f
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-12-21 17:14:32 -0300

    Fazendo o app aproveitar o usuario logado na tela inicial

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -25,6 +25,17 @@ export class AuthService {
         );
     }
 
+    refreshToken() {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/auth/refresh_token`,
+            {},
+            {
+                observe: 'response',
+                responseType: 'text'
+            }
+        );
+    }
+
     successfulLogin(authorizationValue : string) {
         // capturar apenas o token, sem o Bearer
         let tok = authorizationValue.substring(7);
