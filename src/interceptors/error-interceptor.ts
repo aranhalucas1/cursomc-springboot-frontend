0468aeb75498c13e5c207469d81fe086f943b299
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2021-01-12 14:18:50 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
 import { AlertController } from "ionic-angular/components/alert/alert-controller";
+import { FieldMessage } from "../models/fieldmessage";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -33,6 +34,10 @@ export class ErrorInterceptor implements HttpInterceptor {
                     this.handle403();
                     break;
 
+                case 422:
+                    this.handle422(errorObj);
+                    break;
+
                 default:
                     this.handleDefaultError(errorObj);
                     break;
@@ -44,6 +49,20 @@ export class ErrorInterceptor implements HttpInterceptor {
 
             return Observable.throw(errorObj);
         }) as any;
+    }
+
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro de validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ] 
+        });
+        alert.present();
     }
 
     handle403() {
@@ -76,6 +95,15 @@ export class ErrorInterceptor implements HttpInterceptor {
             ] 
         });
         alert.present();
+    }
+
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for(var i = 0; i < messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].message + '</strong></p>';
+        }
+        console.log(s);
+        return s;
     }
 
 }
